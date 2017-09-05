ddcd60e91f8fdb82b8cb89ed4990eeab2927adce
ThiagoAnderson_Ratts <thiago.costa@ratts.com.br>
2017-09-05 10:55:16 -0300

    Starting with the algorithm

--- ITP-PTP/main.c
+++ ITP-PTP/main.c
@@ -2,34 +2,19 @@
 
 int main() {
 
-	// Matriz com 1 linha 30 colunas:
-	/*
-    int juncao_predios[1][30];
-
-    int qtd_predios, i;
-
-    scanf("%s\n", qtd_predios);
+    int predios[10000], i, qtd_predios;
 
 
-    for (i = 0; i<qtd_predios; i++){
-
-    	int predio[1][3];
-    	scanf("%s\n", qtd_predios);
-
+    for(i = 0; i<10000; i++){
+        predios[i] = 0;
     }
 
-    */
-	
-    char nome[1][25];
-	
-	strcpy(nome[0], "s04/09/2017");
+    // for(i = 0; i<10000; i++){
+    //     printf("%d\n", predios[i]);
+    // }
 
+    scanf("%d", &qtd_predios)
 
-    char **mat = split(nome[0],'/');
-
-    for(i=0;i<3;i++){
-	   printf("%s\n",mat[i]);
-    }
 
 
     return 0;
