299cb6da325e3fefef98e31f7ed11b46e48e0fa5
Thiago Anderson Costa <thiagoanderson@A307-14L.imd.local>
2017-09-08 21:45:33 -0300

    Que viagem, mermão

--- ITP-PTP/main.c
+++ ITP-PTP/main.c
@@ -2,17 +2,13 @@
 
 int main() {
 
-    int tam = 30;
+    int tam = 5000;
     int predios[tam], i, j, qtd_predios, inicio, fim, altura;
 
     // Atribuindo 0 a todos os elementos do vetor
     for(i = 0; i<tam; i++){
         predios[i] = 0;
     }
-
-    // for(i = 0; i<tam; i++){
-    //     printf("%d\n", predios[i]);
-    // }
 
     //Quantidade de prédios
     scanf("%d", &qtd_predios);
@@ -24,7 +20,7 @@ int main() {
         scanf("%d %d %d", &inicio, &fim, &altura);
 
         //CRUCIAL: Comparando se o tamanho do prédio inserido é maior do que está gravado no vetor "predios[]" na casa 'i'
-        for (j = inicio; j <= fim; j++){
+        for (j = inicio; j < fim; j++){
             if(predios[j] < altura){
                 predios[j] = altura;
             }
@@ -32,29 +28,28 @@ int main() {
 
     }
 
-     for(i = 0; i<tam; i++){
-        
-        printf("%d ", predios[i]);
-    }
-    
-    printf("\n");
 
-    for(i = 0; i<tam; i++){
-
+    for(i = 0; i<tam-1; i++){
         if(predios[i] != predios[i+1]){
 
-            if(predios[i] < predios[i+1]){
-                printf("%d %d ", (i+1), predios[i+1]);
+            if(i > 2){
+                if(predios[i] < predios[i+1]){
+                    printf("%d %d ", (i+1), predios[i+1]);
+                } else {
+                    printf("%d %d ", (i+1), predios[i+1]);
+                }
             } else {
-                printf("%d %d ", i, predios[i+1]);
+                if(predios[i] < predios[i+1]){
+                    printf("%d %d ", (i+1), predios[i+1]);
+                } else {
+                    printf("%d %d ", i, predios[i+1]);
+                }
             }
-            
+
         }
-        
-        // printf("%d ", predios[i]);
     }
 
-
+    printf("\n");
 
     return 0;
 }
