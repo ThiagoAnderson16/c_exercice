6a5542cb73cb94cf958be0429e486aa750239a69
Thiago Anderson <thiagoanderson16@gmail.com>
2017-11-12 15:04:57 -0300

    Checkpoint 1

---
+++ ITP-PTP/projeto/README.md
@@ -0,0 +1,5 @@
+# Projeto ITP/PTP
+
+Aluno: Thiago Anderson Costa
+
+## Checkpoint 1: Leitura de imagem
--- ITP-PTP/projeto/catarata.c
+++ ITP-PTP/projeto/catarata.c
@@ -1,55 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void leitura_imagem(){
-	char tipo[2], comentario[100];
-	int largura, altura, j, tam_vetor = 1, temp;
-	int* vetor;
-	int* aux;
+// Criando a estrutura da imagem. Inicialmente só precisarei desses 3 valores.
+typedef struct imagem{
+	int largura;
+	int altura;
+	int *vetor_pixels;
+} Imagem;
+
+
+// Função que lerá o arquivo da imagem.
+// Passo tudo como ponteiro para poder ter acesso no main.
+void leitura_imagem(int *largura, int *altura, int *vetor_pixels){
+	
+	char tipo[2], comentario[100]; // Primeira e segunda linha do arquivo PPM
+	int j, tam_vetor = 1, temp; // Contadores
+	//int* vetor_pixels;
+	// Abrindo o arquivo e dizendo que é apenas para leitura
 	FILE *arquivo = fopen("img/Catarata.ppm", "r");
 
-	vetor = malloc(tam_vetor*sizeof(int));
+	// Alocando inicialmente o tamanho do ponteiro
+	vetor_pixels = malloc(tam_vetor*sizeof(int));
 
 	if(arquivo == NULL){
 		printf("Erro na abertura\n");
 	} else {
+		// Esse 'i' representará as linhas do meu arquivo PPM
 		int i = 1;
 		while (!feof(arquivo)) {
+
 			if(i == 1){
+				// Na linha 1 teremos apenas a informação do tipo da imagem 
 				fgets(tipo, 100, arquivo);
 			} else if (i == 2){
+				// Na linha 2 teremos apenas um comentário lá
 				fgets(comentario, 100, arquivo);
 			} else if (i == 3){
-				fscanf(arquivo, "%d %d", &largura, &altura);
+				// Na linha 3 temos a largura e altura da imagem
+				fscanf(arquivo, "%d %d", largura, altura);
 				//break;
 			} else {
+				// Na linha 4 começa os pixels da imagem
 				tam_vetor++;
-				vetor = realloc(vetor, sizeof(int)*tam_vetor);
+				vetor_pixels = realloc(vetor_pixels, sizeof(int)*tam_vetor);
 
             	fscanf(arquivo, "%d", &temp);
-            	vetor[i-4] = temp;
+            	vetor_pixels[i-4] = temp;
 			}
+
 			i++;
 		}
 	}
 
+	// Fechando o arquivo
 	fclose(arquivo);
 
+	// Esses prints são apenas para minha verificação. Se o que foi programado está correto
 	printf("%s", tipo);
 	printf("%s", comentario);
-	printf("%d %d\n", largura, altura);
+	printf("%d %d\n", largura[0], altura[0]);
 
-	for(int i = 2311150; i<2311157; i++){
-		printf("%d\n", vetor[i]);
+	for(int i = 0; i<5; i++){
+		printf("%d\n", vetor_pixels[i]);
 	}
 
-	free(vetor);	
+	// Não dei o free no vetor pois quero poder acessá-lo no main
+	//free(vetor_pixels);
 
 }
 
 int main(){
+	int pixels, largura = 0, altura = 0;
 
-	leitura_imagem();
+	// Inicializando ("instanciando") a imagem
+	Imagem imagem = {largura, altura, &pixels};
+
+	// Passando os valores para função de leitura de imagem
+	leitura_imagem(&imagem.largura, &imagem.altura, &imagem.vetor_pixels[0]);
+
+	// Imprimindo os valores para veriicar se realmente posso tratá-los a partir da estrutura definida
+	printf("%d %d %d\n", imagem.largura, imagem.altura, imagem.vetor_pixels[0]);
+
+	/* 
+	Problema encontrado: nessa print acima, em "imagem.vetor_pixels[0]", o valor esperado 
+	era o primeiro pixels da imagem PPM lida, porém o que é impresso é 0.
+	*/
 	
 	return 0;
 }
